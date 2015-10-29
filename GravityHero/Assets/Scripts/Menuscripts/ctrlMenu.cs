3e6ba96313515a524ceeada57802f13ff20dcfc8
Galleom <Mr.galleom@hotmail.com>
2015-10-29 17:53:48 -0400

    menu1
    menu básico

---
+++ GravityHero/Assets/Scripts/Menuscripts/ctrlMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ctrlMenu : MonoBehaviour {
+    //public string playScene;
+	// Use this for initialization
+
+	public void Play(){
+		Application.LoadLevel("scene_base");
+		//Debug.Log ("Nova Cena");
+	}
+
+	public void Option(){
+		//Application.LoadLevel("");
+		Debug.Log ("Tela de Opcoes");
+	}
+	public void Exit(){
+		//Application.Quit();
+		Debug.Log ("Sair");
+	}
+}
